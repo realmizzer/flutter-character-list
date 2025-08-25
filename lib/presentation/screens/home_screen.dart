ca8c9a6823448b2be637e41214d8dad233257737
realmizzer <diamondAlma32@gmail.com>
2025-08-25 18:31:13 +0700

    feat: bottom navbar

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_character_list/presentation/screens/characters_screen.dart';
+import 'package:flutter_character_list/presentation/screens/favorites_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+  final List<Widget> _screens = [CharactersScreen(), FavoritesScreen()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(children: [Text('Hello, Flutter!')]),
+      body: _screens.elementAt(_currentIndex),
+      bottomNavigationBar: NavigationBar(
+        destinations: [
+          NavigationDestination(icon: Icon(Icons.person), label: 'Characters'),
+          NavigationDestination(icon: Icon(Icons.star), label: 'Favorites'),
+        ],
+        onDestinationSelected: (value) {
+          setState(() {
+            _currentIndex = value;
+          });
+          print(_currentIndex);
+        },
+        selectedIndex: _currentIndex,
+      ),
     );
   }
 }
